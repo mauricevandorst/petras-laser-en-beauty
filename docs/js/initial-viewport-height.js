05caba5d6559963ffd5705a9b87c852689c834d4
Maurice van Dorst <maurice.vandorst@retail.nl>
2026-03-23 14:29:41 +0100

    View port fix

--- docs/js/initial-viewport-height.js
+++ docs/js/initial-viewport-height.js
@@ -1,4 +1,4 @@
-const initialViewportHeight = window.innerHeight;
+const initialViewportHeight = window.initialViewportHeight || window.visualViewport?.height || window.innerHeight;
 
 if (Number.isFinite(initialViewportHeight) && initialViewportHeight > 0) {
   document.documentElement.style.setProperty(
